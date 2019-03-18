0fd8bc567571b7468a679d6e208c34db5bf1d532
nyuqiu <tomeczek110@gmail.com>
2019-03-18 14:58:30 +0000

    sss

--- src/main/java/com/crud/tasks/trello/config/TrelloConfig.java
+++ src/main/java/com/crud/tasks/trello/config/TrelloConfig.java
@@ -1,6 +1,5 @@
 package com.crud.tasks.trello.config;
 
-import org.springframework.stereotype.Component;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
