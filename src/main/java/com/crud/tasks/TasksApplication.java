d29f7bce3a23da423908f34a347cf684d1e14b41
nyuqiu <tomeczek110@gmail.com>
2019-03-18 15:18:10 +0000

    ccc

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,9 +2,10 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 //public class TasksApplication extends SpringBootServletInitializer {
 public class TasksApplication{
     public static void main(String[] args) {
